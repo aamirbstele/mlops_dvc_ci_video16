f966f8fbbf333549a2bac2d6b37899bbf4d90a23
Aamir Sattar <aamir.sattar@puresquare.com>
2025-01-20 13:45:30 +0500

    new update mlflow

--- src/model/model_eval.py
+++ src/model/model_eval.py
@@ -22,14 +22,14 @@ import os
 
 ##mlflow.set_experiment("Final_Model")
 
-dagshub_token = os.getenv("DAGSHUB_TOKEN")
-if not dagshub_token:
-    raise EnvironmentError("DAGSHUB_TOKEN environment variable is not set")
+#dagshub_token = os.getenv("DAGSHUB_TOKEN")
+#if not dagshub_token:
+#    raise EnvironmentError("DAGSHUB_TOKEN environment variable is not set")
 
-os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+#os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+#os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-dagshub_url = "https://dagshub.com"
+dagshub_url = "http://localhost:5000"
 repo_owner = "aamirbstele"
 repo_name = "mlops_dvc_ci_video16"
 mlflow.set_tracking_uri(f"{dagshub_url}/{repo_owner}/{repo_name}.mlflow")
